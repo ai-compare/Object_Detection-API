62cf5fc86f56eee0dddb1135cf05c8ff41cbda33
ai-compare <67952486+ai-compare@users.noreply.github.com>
2020-10-21 10:33:48 +0200

    Update AI-Compare-ObjectDetection-python-request.py

--- AI-Compare-ObjectDetection-python-request.py
+++ AI-Compare-ObjectDetection-python-request.py
@@ -3,13 +3,13 @@ import requests
 # You can find the documentation here : https://documenter.getpostman.com/view/10011301/SzmiVFqh?version=latest#6571813e-96e4-4e4a-8603-41cd739bdd3b
 
 #Enter your API Token
-headers = {'x-access-token': 'Enter your API Key'} #You can get your free API token here https://www.ai-compare.com/accounts/login/?next=/my_apis
+headers = {  'Authorization': 'Bearer your API Key'} #You can get your free API token here https://www.ai-compare.com/accounts/login/?next=/my_apis
 
 # Select API
-url = 'https://www.ai-compare.com/api/v1/vision/create/compare/object_detection'
+url = 'https://www.ai-compare.com/v1/pretrained/vision/object_detection'
 
 # Select providers, and objects to detect
-payload = {'providers': '[\'google_cloud\', \'cognitives_service\', \'aws\']','objects_to_find': ''}
+payload = {'providers': '[\'google_cloud\', \'cognitives_service\', \'aws\', \'ibm\']','objects_to_find': ''}
 
 # Select file to test
 files = [  ('files', open('Picture/example.jpg','rb'))]
